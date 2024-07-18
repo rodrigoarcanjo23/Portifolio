443afc94bc430b2c4893e77ed38e303f50473e58
Rodrigo <rodrigoarcanjo690@gmail.com>
2024-07-18 13:12:44 -0300

    updade 2.0

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -2,32 +2,32 @@ const Services_Data = [
     {
         s_no: "01",
         s_name: "Web design",
-        s_desc: "Um web designer é responsável por criar o visual e a usabilidade de sites. Utiliza habilidades de design gráfico e conhecimentos técnicos para desenvolver layouts atrativos e funcionais, garantindo uma experiência de usuário positiva."
+        s_desc: "Um web designer tem a função de conceber o visual e a usabilidade de websites. Combinando habilidades em design gráfico e conhecimento técnico..."
     },
     {
         s_no: "02",
         s_name: "Graphics design",
-        s_desc: "O design gráfico envolve a criação de elementos visuais para comunicar mensagens. Inclui a concepção de logotipos, identidade visual, materiais de marketing, e publicidade, utilizando cores, tipografia e imagens de forma criativa e eficaz."
+        s_desc: "Cria elementos visuais para comunicação eficaz: logotipos, identidade visual, materiais de marketing e publicidade, usando cores, tipografia e imagens de forma criativa..."
     },
     {
         s_no: "03",
         s_name: "Social media",
-        s_desc: "elaboro conteúdos relevantes, que interagem com seguidores, monitorando métricas de engajamento e implemento estratégias para aumentar a visibilidade e interação da marca nas redes sociais."
+        s_desc: "elaboro conteúdos relevantes, que interagem com seguidores, monitorando métricas de engajamento e implemento estratégias para aumentar a visibilidade..."
     },
     {
         s_no: "04",
         s_name: "App design",
-        s_desc: "App design refere-se ao processo de criação da interface e experiência do usuário (UI/UX) para aplicativos móveis. Foca na usabilidade intuitiva, navegação eficiente e design visual atraente, garantindo que o aplicativo seja funcional e fácil de usar."
+        s_desc: "App design refere-se ao processo de criação da interface e experiência do usuário (UI/UX) para aplicativos móveis..."
     },
     {
         s_no: "05",
         s_name: "Digital marketing",
-        s_desc: "O marketing digital abrange todas as estratégias e atividades de promoção de uma marca, produto ou serviço na internet. Inclui SEO (otimização para mecanismos de busca), campanhas de PPC (pagamento por clique), marketing de conteúdo, email marketing, entre outras técnicas para alcançar e engajar o público-alvo online"
+        s_desc: "O marketing digital abrange todas as estratégias e atividades de promoção de uma marca, produto ou serviço na internet..."
     },
     {
         s_no: "06",
         s_name: "Diagramação",
-        s_desc: "A diagramação é o processo de organização visual de elementos textuais e gráficos em um layout. É fundamental em publicações como livros, revistas, jornais e documentos corporativos, garantindo que o conteúdo seja apresentado de maneira clara, organizada e esteticamente agradável."
+        s_desc: "A diagramação é o processo de organização visual de elementos textuais e gráficos em um layout..."
     },
 ]
 
